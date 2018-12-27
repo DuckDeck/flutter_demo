643201e1c56eb98c70338394f47614b83a084d85
“stanhu” <3421902@qq.com>
2018-12-27 14:25:37 +0800

    process code

--- lib/Novel/Model/SectionInfo.dart
+++ lib/Novel/Model/SectionInfo.dart
@@ -1,6 +1,8 @@
-
+import 'dart:typed_data';
 import 'package:html/parser.dart';
 import 'package:dio/dio.dart';
+import 'dart:io';
+import 'package:gbk2utf8/gbk2utf8.dart';
 class SectionInfo{
   const SectionInfo({
     this.name,
@@ -11,7 +13,10 @@ class SectionInfo{
   static Future<List<SectionInfo>> getSectionList(String link) async{
      try{
         Dio dio = new Dio();
-       Response<String> response=await dio.get(link);
+        Response<String> response=await dio.get(link);
+ 
+        
+     //困为搞不定GBK转UTF8，所以不做了
        var docu = parse(response.data);
        var novels = List<SectionInfo>();
        var items = docu.querySelector('div#list').getElementsByTagName("dd");
