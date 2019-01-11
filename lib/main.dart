94234f5b40def1cec0f2edfca6f3c62c3058b7d0
“stanhu” <3421902@qq.com>
2019-01-11 15:26:38 +0800

    process code

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to flutter',
       theme: new ThemeData(primaryColor: Colors.purple),
-      home: new NovelSearch()
+      home: new RandomWords()
       );
     
   }
