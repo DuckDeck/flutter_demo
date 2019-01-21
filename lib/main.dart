c4450c447318dd6686a4f7973cf860d4041d35e6
“stanhu” <3421902@qq.com>
2019-01-21 16:51:04 +0800

    procoss code

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to flutter',
       theme: new ThemeData(primaryColor: Colors.purple),
-      home: new RandomWords()
+      home: new BottomNavigation()
       );
     
   }
@@ -35,6 +35,9 @@ class MyApp extends StatelessWidget {
 
 
 class TutorialHome extends StatelessWidget{
+
+
+
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
@@ -129,3 +132,61 @@ class CounterIncrementor extends StatelessWidget{
     }
 }
 
+
+class BottomNavigation extends StatefulWidget {
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+   final _BottomNavigationColor = Colors.blue;
+   int _currentIndex = 0;
+   List<Widget> list = List();
+   
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      list
+      ..add(HomeScreen(name: "Home",))
+      ..add(HomeScreen(name: "Rmail",))
+      ..add(HomeScreen(name: "Pages",))
+      ..add(HomeScreen(name: "Airplay",));
+    }
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      body: list[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,color:_BottomNavigationColor),title: Text('Home',style:TextStyle(color:_BottomNavigationColor))),
+          BottomNavigationBarItem(icon: Icon(Icons.email,color:_BottomNavigationColor),title: Text('Email',style:TextStyle(color:_BottomNavigationColor))),
+          BottomNavigationBarItem(icon: Icon(Icons.pages,color:_BottomNavigationColor),title: Text('Pages',style:TextStyle(color:_BottomNavigationColor))),
+          BottomNavigationBarItem(icon: Icon(Icons.airplay,color:_BottomNavigationColor),title: Text('Airplay',style:TextStyle(color:_BottomNavigationColor)))
+        ],
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index){
+          setState(() {
+                      _currentIndex = index;
+                    });
+        },
+      ),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
+
+  final String name;
+
+  const HomeScreen({Key key,this.name}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(this.name),),
+      body: Center(child: Text(this.name),),
+    );
+  }
+}
