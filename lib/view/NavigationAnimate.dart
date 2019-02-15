7c0d0147ed3b8cd4c822b7ebbecb5d176314704d
“stanhu” <3421902@qq.com>
2019-02-15 10:17:02 +0800

    route transtion

--- lib/view/NavigationAnimate.dart
+++ lib/view/NavigationAnimate.dart
@@ -10,7 +10,7 @@ class FirstPage extends StatelessWidget {
         // Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext conext){
         //   return SecondPage();
         // }));
-        Navigator.of(context).push(CustomRoute(SecondPage()));
+        Navigator.of(context).push(SlideTransitionRoute(SecondPage()));
       },
     ),),
     );
@@ -35,14 +35,82 @@ class SecondPage extends StatelessWidget {
 }
 
 
-class CustomRoute extends PageRouteBuilder{
+class FadeTransitionRoute extends PageRouteBuilder{
   final Widget widget;
-  CustomRoute(this.widget):super(transitionDuration:const Duration(seconds: 1),
-  pageBuilder:(BuildContext context,Animation<double> animation1,Animation<double> animation2){
+  FadeTransitionRoute(this.widget):super(transitionDuration:const Duration(seconds: 1),
+  pageBuilder:(BuildContext context,Animation animation1,Animation animation2){
     return widget;
   },
-  transitionsBuilder:(BuildContext context,Animation<double> animation1,Animation<double> animation2,Widget child){
-    return FadeTransition(opacity: Tween(begin: 0,end: 1.0).animate(CurvedAnimation(parent: animation1,curve:Curves.fastOutSlowIn)),
-    child: child,);
-  });
+  transitionsBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2,
+          Widget child){
+            return FadeTransition(
+              opacity: Tween(begin:0.0,end :1.0).animate(CurvedAnimation(
+                  parent:animation1,
+                  curve:Curves.fastOutSlowIn
+              )),
+              child: child,
+            );
+        }  );
 }
+
+class ScaleTransitionRoute extends PageRouteBuilder{
+  final Widget widget;
+  ScaleTransitionRoute(this.widget):super(transitionDuration:const Duration(seconds: 1),
+  pageBuilder:(BuildContext context,Animation animation1,Animation animation2){
+    return widget;
+  },
+  transitionsBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2,
+          Widget child){
+            return ScaleTransition(
+              scale: Tween(begin: 0.0,end: 1.0).animate(CurvedAnimation(
+                parent: animation1,
+                curve: Curves.fastOutSlowIn
+              )),
+              child: child,
+            );
+        }  );
+}
+
+class RotationTransitionRoute extends PageRouteBuilder{
+  final Widget widget;
+  RotationTransitionRoute(this.widget):super(transitionDuration:const Duration(seconds: 1),
+  pageBuilder:(BuildContext context,Animation animation1,Animation animation2){
+    return widget;
+  },
+  transitionsBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2,
+          Widget child){
+             return RotationTransition(turns: Tween(begin: 0.0,end: 1.0).animate(CurvedAnimation(parent: animation1,curve: Curves.fastOutSlowIn)),
+             child: ScaleTransition(scale: Tween(begin: 0.0,end: 1.0).animate(CurvedAnimation(parent: animation1,curve: Curves.fastOutSlowIn)),
+             child: child,),
+             );
+        }  );
+}
+
+class SlideTransitionRoute extends PageRouteBuilder{
+  final Widget widget;
+  SlideTransitionRoute(this.widget):super(transitionDuration:const Duration(seconds: 1),
+  pageBuilder:(BuildContext context,Animation animation1,Animation animation2){
+    return widget;
+  },
+  transitionsBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2,
+          Widget child){
+            return SlideTransition(position: 
+            Tween<Offset>(begin: Offset(1.0, 0.0),end: Offset(0.0, 0.0)).animate(CurvedAnimation(parent: animation1,curve: Curves.easeInOutCirc)),
+            child: child,);
+        }  );
+}
+
+
+
