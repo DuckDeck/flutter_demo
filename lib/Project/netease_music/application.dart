23f9361fd75940da95cefc616821a35aa4b5702d
“stanhu” <3421902@qq.com>
2019-11-05 11:00:49 +0800

    process cod

--- lib/Project/netease_music/application.dart
+++ lib/Project/netease_music/application.dart
@@ -1,9 +1,8 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:netease_cloud_music/route/navigate_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:flutter_demo/Project/netease_music/route/navigate_service.dart';
 class Application{
   static Router router;
   static GlobalKey<NavigatorState> key = GlobalKey();
