92b70bae639565a92667098f1e3edd3d8edc16f6
“stanhu” <3421902@qq.com>
2019-11-15 15:22:39 +0800

    reset the project

--- android/app/src/main/kotlin/com/example/flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_demo/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.flutter_demo
 
-import android.os.Bundle
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
