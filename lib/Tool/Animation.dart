46e5bc8346bed3509f0d17ed04e2ee6d7367c44f
“stanhu” <3421902@qq.com>
2019-11-19 18:08:15 +0800

    process code

--- lib/Tool/Animation.dart
+++ lib/Tool/Animation.dart
@@ -191,3 +191,34 @@ class _StaggerRouteState extends State<StaggerRoute> with TickerProviderStateMix
   }
 
 }
+
+class AnimatedSwitcherCounterRoute extends StatefulWidget {
+  const AnimatedSwitcherCounterRoute({Key key}):super(key:key);
+  @override
+  _AnimatedSwitcherCounterRouteState createState() => _AnimatedSwitcherCounterRouteState();
+}
+
+class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRoute> {
+  int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedSwitcher(duration: const Duration(milliseconds: 500),
+          transitionBuilder: (Widget child,Animation<double> animation){
+            return ScaleTransition(child: child,scale: animation,);
+          },
+          child: Text('$_count',key: ValueKey<int>(_count),style: Theme.of(context).textTheme.display1,),
+          ),
+          RaisedButton(child: const Text("+1"),onPressed: (){
+            setState(() {
+              _count += 1;
+            });
+          },)
+        ],
+      ),
+    );
+  }
+}
--- lib/UI/menu.dart
+++ lib/UI/menu.dart
@@ -8,7 +8,7 @@ import 'package:flutter_demo/UI/Shopping.dart';
 import 'package:flutter_demo/UI/TurnBox.dart';
 import 'package:flutter_demo/Tool/Animation.dart';
 const items = [
-  "无限滚动的ListView","下面有悬浮按钮的BottomBar","搜索条","购物车","TuenBox","动画效果","Hero动画","Stagger动画"
+  "无限滚动的ListView","下面有悬浮按钮的BottomBar","搜索条","购物车","TuenBox","动画效果","Hero动画","Stagger动画","计数器动画"
 ];
 
 class LayoutListPage extends StatefulWidget {
@@ -72,6 +72,11 @@ void gotoPage(int index,BuildContext context) {
            case 7:
              Navigator.of(context).push(new CupertinoPageRoute(builder: (BuildContext context){
                return StaggerRoute();
+              }));
+          break;
+          case 8:
+             Navigator.of(context).push(new CupertinoPageRoute(builder: (BuildContext context){
+               return AnimatedSwitcherCounterRoute();
               }));
           break;
         default:
