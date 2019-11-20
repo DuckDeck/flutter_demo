30de2e45233decf618ba98d417858dbbf7d23617
“stanhu” <3421902@qq.com>
2019-11-20 14:53:34 +0800

    加点动画

--- lib/Tool/Animation.dart
+++ lib/Tool/Animation.dart
@@ -172,7 +172,6 @@ class _StaggerRouteState extends State<StaggerRoute> with TickerProviderStateMix
       await _controller.forward().orCancel;
       await _controller.reverse().orCancel;
     } on TickerCanceled{
-
     }
   }
   @override
@@ -208,9 +207,13 @@ class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRo
         children: <Widget>[
           AnimatedSwitcher(duration: const Duration(milliseconds: 500),
           transitionBuilder: (Widget child,Animation<double> animation){
-            return ScaleTransition(child: child,scale: animation,);
+            var tween = Tween<Offset>(begin: Offset(1,0),end: Offset(0,0));
+            //return ScaleTransition(child: child,scale: animation,); //大小变化的动画
+            //return SlideTransition(child: child,position: tween.animate(animation),); //左右移动的动画，但是有问题
+            //return CustomSlideTransition(child: child,position: tween.animate(animation),); //用了自定义的动画，看起来好了很多
+            return SlideTransitionX(child: child,direction: AxisDirection.down,position: animation,); //从上往下的
           },
-          child: Text('$_count',key: ValueKey<int>(_count),style: Theme.of(context).textTheme.display1,),
+          child: Text('$_count',key: ValueKey<int>(_count)),
           ),
           RaisedButton(child: const Text("+1"),onPressed: (){
             setState(() {
@@ -221,4 +224,69 @@ class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRo
       ),
     );
   }
+}
+
+class CustomSlideTransition extends AnimatedWidget {
+  final bool transformHitTests;
+  final Widget child;
+  CustomSlideTransition({Key key,@required Animation<Offset> position,this.transformHitTests = true,this.child}) : assert(position != null) ,super(key:key,listenable:position);
+
+  Animation<Offset> get position =>listenable;
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = position.value;
+    if(position.status == AnimationStatus.reverse){
+      offset = Offset(-offset.dx,offset.dy);
+    }
+    return FractionalTranslation(translation: offset,transformHitTests: transformHitTests,child: child,);
+  }
+}
+
+
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX({Key key,@required Animation<double> position,this.transformHitTests = true,this.direction = AxisDirection.down,this.child}) 
+  :assert(position != null),super(key:key,listenable:position){
+    switch(direction){
+      case AxisDirection.up:
+      _tween = Tween(begin: Offset(0,1),end: Offset(0, 0));
+      break;
+      case AxisDirection.right:
+      _tween = Tween(begin: Offset(-1,0),end: Offset(0, 0));
+      break;
+      case AxisDirection.down:
+      _tween = Tween(begin: Offset(0,-1),end: Offset(0, 0));
+      break;
+      case AxisDirection.left:
+      _tween = Tween(begin: Offset(1,0),end: Offset(0, 0));
+      break;
+    }
+  }
+   final bool transformHitTests;
+  Animation<double> get position =>listenable;
+  final Widget child;
+  final AxisDirection direction;
+  Tween<Offset> _tween;
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if(position.status == AnimationStatus.reverse){
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy) ;
+          break;
+          case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy) ;
+          break;
+          case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy) ;
+          break;
+          case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy) ;
+          break;
+        default:
+      }
+    }
+    return FractionalTranslation(translation: offset,transformHitTests: transformHitTests,child: child,);
+  }
+  
 }
