982bdcac07d7e3bb51a79316515ff25a45a242a4
“stanhu” <3421902@qq.com>
2019-12-10 14:48:50 +0800

    process code

--- lib/Project/4KImage/main_image_list.dart
+++ lib/Project/4KImage/main_image_list.dart
@@ -1,63 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'model.dart';
 class MainImageList extends StatefulWidget {
   @override
   _MainImageListState createState() => _MainImageListState();
 }
 
 class _MainImageListState extends State<MainImageList>  with SingleTickerProviderStateMixin {
-  PageController controller;
-  TabController tabController;
+
+  var titles = [Cat(name: "最新",urlSegment: "new"),Cat(name: "风景",urlSegment: "4kfengjing"),
+  Cat(name: "美女",urlSegment: "4kmeinv"),Cat(name: "游戏",urlSegment: "4kyouxi"),
+  Cat(name: "动漫",urlSegment: "4kdongman"),Cat(name: "影视",urlSegment: "4kyingshi"),
+  Cat(name: "明星",urlSegment: "4kmingxing"),Cat(name: "明星",urlSegment: "4kqiche"),
+  Cat(name: "动物",urlSegment: "4kdongwu"),Cat(name: "汽车",urlSegment: "4krenwu"),
+  Cat(name: "美食",urlSegment: "4kmeishi"),Cat(name: "宗教",urlSegment: "4kzongjiao"),
+  Cat(name: "背景",urlSegment: "4kbeijing"),];
   var imgs = [
     Image.asset("Images/head1.jpg"),
     Image.asset("Images/head2.jpg"),
     Image.asset("Images/head3.jpg"),
     Image.asset("Images/head3.jpg"),
+    Image.asset("Images/head1.jpg"),
+    Image.asset("Images/head2.jpg"),
+    Image.asset("Images/head3.jpg"),
+    Image.asset("Images/head3.jpg"),
+    Image.asset("Images/head1.jpg"),
+    Image.asset("Images/head2.jpg"),
+    Image.asset("Images/head3.jpg"),
+    Image.asset("Images/head3.jpg"),
+    Image.asset("Images/head1.jpg"),
+
   ];
-  var tabs = [Tab(text: "123",),Tab(text: "123",),Tab(text: "123",),Tab(text: "123",)];
+
   @override
   void initState() {
     super.initState();
-    controller = PageController(initialPage: 4);
-    tabController = TabController(length: 4,vsync: this);
+    
   }
 
   @override
   Widget build(BuildContext context) {
     ScreenUtil.instance = ScreenUtil(width: 750,height: 1334)..init(context);
-    return Container(
-      child:  TabBarView(
-            controller: tabController,
-            children: tabs.map((Tab tab) {
-            return Center(
-              child: new Text(tab.text),
-            );
-          }).toList()),
+    return DefaultTabController(
+      length: 13,
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Tabbed Appbar"), bottom: TabBar(
+          isScrollable: true,
+          tabs:titles.map((title){
+            return Tab(text: title.name,);
+          }).toList(),
+        ),),
+        body: TabBarView(
+          children: imgs.map((img){
+            return Padding(padding: EdgeInsets.all(16.0), child:img);
+          }).toList()
+        ),
+      ),
     );
   }
 }
-
-
-// Column(
-//         children: <Widget>[
-//           TabBarView(
-            
-//             controller: tabController,
-//             children: tabs.map((Tab tab) {
-//             return Center(
-//               child: new Text("内容："+tab.text),
-//             );
-//           }).toList()),
-//           Container(
-//             height: ScreenUtil.getInstance().setHeight(1234),
-//             child: PageView.builder(
-//             scrollDirection: Axis.horizontal,
-//             controller: controller,
-//             itemBuilder: (context, index) {
-//               return imgs[index];
-//             },
-//             itemCount: imgs.length,
-//           ),
-//           )
-//         ],
-//       ),
---
+++ lib/Project/4KImage/model.dart
@@ -0,0 +1,5 @@
+class Cat {
+  Cat({this.name,this.urlSegment});
+  final String name;
+  final String urlSegment;
+}
