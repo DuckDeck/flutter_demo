8768a2482aed2d3da59bba28e27bc3a8cccebc11
“stanhu” <3421902@qq.com>
2019-12-25 09:12:38 +0800

    process code

--- lib/Foundation/Reflect.dart
+++ lib/Foundation/Reflect.dart
@@ -1,9 +1,24 @@
+
 import 'package:flutter/material.dart';
+
 class Reflect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+
+   
+
+    return Scaffold(
+      appBar: AppBar(title: Text("反射"),),
+      body: Center(
+        child: Text("Flutter 禁止使用反射。。这个用不了"),
+      ),
     );
   }
+}
+
+class Test {
+  var name = "";
+  var age = 0;
+  
 }
