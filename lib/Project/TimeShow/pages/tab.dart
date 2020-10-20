702c33c10ebd5bf7a5a9ceec1295a5648d06164c
DuckDeck <3421902@qq.com>
2020-10-20 17:59:47 +0800

    add tab.dart

--- lib/Project/TimeShow/pages/tab.dart
+++ lib/Project/TimeShow/pages/tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/Project/TimeShow/components/common/tab_bar.dart';
 
 class TabPage extends StatefulWidget {
   static final globalKey = GlobalKey<_TabPageState>();
@@ -8,6 +9,21 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  final _nabigationLKeys = TsTabBar.tabs.map<GlobalKey<NavigatorState>>((e) => GlobalKey<NavigatorState>()).toList();
+  final _focusScopeNodes = TsTabBar.tabs.map<FocusScopeNode>((e) => FocusScopeNode()).toList();
+  var _tabIndex = 0;
+  void switchTab(int index){
+    setState(() {
+      _tabIndex = index;
+    });
+    FocusScope.of(context).setFirstFocus(_focusScopeNodes[index]);
+  }
+
+Future<bool> _onWillPop() async{
+  final maybePop = await _nabigationLKeys[_tabIndex].currentState.maybePop();
+  return Future.value(!maybePop);
+}
+
   @override
   Widget build(BuildContext context) {
     return Container(
