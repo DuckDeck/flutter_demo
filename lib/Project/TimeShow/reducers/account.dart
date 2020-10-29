ca890d9e436f34b5f335fc04bc25ba51585bbc94
DuckDeck <3421902@qq.com>
2020-10-29 10:23:35 +0800

    delete timeshow

--- lib/Project/TimeShow/reducers/account.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_demo/Project/TimeShow/models/state/account.dart';
-import 'package:redux/redux.dart';
-
-final accountReducer = combineReducers<AccounState>()
--- lib/Project/TimeShow/utils/json.dart
+++
@@ -1,8 +0,0 @@
-Duration durationFromMillseconds(int milliseconds)=>
-  Duration(milliseconds: milliseconds);
-
-int durationToMilliseconds(Duration duration) => duration.inMilliseconds;
-
-DateTime datetimeFromEpochMilliseconds(int us) => 
-  DateTime.fromMillisecondsSinceEpoch(us);
-int dateTimeToEpochMillseconds(DateTime dateTime) => dateTime.millisecondsSinceEpoch;
