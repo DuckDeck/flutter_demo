219ee24b243b0bfe786f88cdb1c43219cd6552b0
DuckDeck <3421902@qq.com>
2020-11-18 13:43:30 +0800

    process code

---
+++ lib/Project/Blog/articleDetail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ArtilDetailPage extends StatefulWidget {
+  @override
+  _ArtilDetailPageState createState() => _ArtilDetailPageState();
+}
+
+class _ArtilDetailPageState extends State<ArtilDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
