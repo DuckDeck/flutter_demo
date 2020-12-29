675b776f10a8835c2cf35001fbde01842ae17ef4
DuckDeck <3421902@qq.com>
2020-12-29 18:17:33 +0800

    process code

--- lib/Project/Blog/main.dart
+++ lib/Project/Blog/main.dart
@@ -1,12 +1,16 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/Project/Blog/Model/ArticleInfo.dart';
+import 'package:flutter_demo/Project/Blog/Model/UserInfo.dart';
 import 'package:flutter_demo/Project/Blog/UI/RefreshAndLoadMore.dart';
 import 'package:flutter_demo/Project/Blog/UI/articleCell.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'searchContent.dart';
+
 class ZoeBlogPage extends StatefulWidget {
   @override
   _ZoeBlogPageState createState() => _ZoeBlogPageState();
@@ -32,7 +36,14 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text("ZOE Blog"),
-        actions: [IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: gotoSearch)],
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: gotoSearch)
+        ],
         leading: Builder(builder: (context) {
           return IconButton(
               icon: Icon(Icons.person),
@@ -44,7 +55,10 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
       drawer: LeftMenu(),
       body: Container(
           child: RefreshAndLoadMore(
-              controller: rc, refreshFun: initData,loadMoreFun: loadMore, children: buildList())),
+              controller: rc,
+              refreshFun: initData,
+              loadMoreFun: loadMore,
+              children: buildList())),
     );
   }
 
@@ -63,7 +77,7 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
     });
   }
 
-  void loadMore() async{
+  void loadMore() async {
     index += 1;
     final result = await ArticleInfo.indexPage(index);
     rc.loadComplete();
@@ -72,14 +86,13 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
       return;
     }
     final data = result.data as List<ArticleInfo>;
-    if(data.length <= 0){
+    if (data.length <= 0) {
       rc.loadNoData();
       return;
     }
     setState(() {
       articles.addAll(data);
     });
-
   }
 
   List<Widget> buildList() {
@@ -89,9 +102,9 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
         autoplay: true,
         itemCount: banners.length,
         pagination: new SwiperPagination(),
-        onTap: (int index){
-              Navigator.of(context).pushNamed("/project/blog/article",arguments: banners[index]);
-
+        onTap: (int index) {
+          Navigator.of(context)
+              .pushNamed("/project/blog/article", arguments: banners[index]);
         },
         itemBuilder: (BuildContext context, int bannerIndex) {
           return Stack(
@@ -126,16 +139,16 @@ class _ZoeBlogPageState extends State<ZoeBlogPage> {
   }
 
   void gotoSearch() {
-   
-    showSearch(context: context,delegate: SearchBarDelegate());
+    showSearch(context: context, delegate: SearchBarDelegate());
   }
 }
 
 class LeftMenu extends StatelessWidget {
-  const LeftMenu({
+  LeftMenu({
+    UserInfo userinfo,
     Key key,
   }) : super(key: key);
-
+  UserInfo userInfo;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -148,16 +161,42 @@ class LeftMenu extends StatelessWidget {
               padding: const EdgeInsets.only(top: 38.0),
               child: Row(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Image.asset(
-                      "Images/1.jpg",
-                      width: 80,
-                    ),
-                  ),
-                  Text(
-                    "data",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                     CachedNetworkImage(
+                        imageUrl: "http://bqbbq.com/static/img/zoe_logo.jpg",
+                        fit: BoxFit.cover,
+                        width: 300,
+                      ),
+                      Center(
+                        child: ClipRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),
+                            child: Opacity(
+                              opacity: 0.5,
+                              child: Container(child: Text(" "),width: 300,height:200),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 20,
+                        child: ClipOval(
+                          child: CachedNetworkImage(
+                              imageUrl: userInfo == null ? "" : userInfo.headImage,
+                              width: 64,
+                              height: 64,
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) =>
+                                  Image.asset("Images/placeholder_head.jpg")),
+                        ),
+                      ),
+                      Positioned(
+                        left: 100,
+                        child:Text("未登录",style: TextStyle(fontSize: 25),)
+                      ),
+                    ],
                   )
                 ],
               ),
@@ -171,7 +210,7 @@ class LeftMenu extends StatelessWidget {
                 ),
                 ListTile(
                   leading: const Icon(Icons.settings),
-                  title: const Text("Manage Account"),
+                  title: const Text("系统设置"),
                 )
               ],
             ))
