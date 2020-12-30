1688fbe10e9b6a8729c3b238bb3929f6679bdb57
DuckDeck <3421902@qq.com>
2020-12-30 10:42:20 +0800

    process code

--- lib/Project/Blog/blogLogin.dart
+++ lib/Project/Blog/blogLogin.dart
@@ -12,10 +12,7 @@ class _BlogLoginPageState extends State<BlogLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        alignment: Alignment.center, children: [
-        
+      body: Stack(fit: StackFit.expand, alignment: Alignment.center, children: [
         CachedNetworkImage(
           imageUrl:
               "http://pic.netbian.com/uploads/allimg/201222/001204-1608567124c15d.jpg",
@@ -28,17 +25,85 @@ class _BlogLoginPageState extends State<BlogLoginPage> {
               filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
               child: Opacity(
                 opacity: 0.5,
-                child: Container(child: Text(" "), width: 360, height: 200),
+                child: Container(child: Text(" "), width: 360, height: 240),
               ),
             ),
           ),
         ),
         Positioned(
-          left: 30,
-          top: 30,
-          child: GestureDetector(
-          child: IconButton(icon: Icon(Icons.arrow_back),),
-        )),
+            left: 30,
+            top: 30,
+            child: GestureDetector(
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+              ),
+            )),
+        Center(
+            child: Container(
+          width: 360,
+          height: 200,
+          child: Column(
+            children: [
+              TextField(
+                  decoration: InputDecoration(
+                labelText: "用户名",
+                hintText: "用户名或者邮箱",
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+              )),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "密码",
+                  hintText: "你的登录密码",
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    child: Text("注册",
+                        style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline)),
+                  ),
+                  GestureDetector(
+                    child: Text("忘记密码",
+                        style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Container(
+                  width: 240,
+                  child: Text(
+                    "登录",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20,),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ))
       ]),
     );
   }
