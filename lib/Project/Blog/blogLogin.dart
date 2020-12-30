f1e59766c477e53ef495765ad718e644cc1aff79
DuckDeck <3421902@qq.com>
2020-12-30 14:01:55 +0800

    process code

--- lib/Project/Blog/blogLogin.dart
+++ lib/Project/Blog/blogLogin.dart
@@ -10,6 +10,7 @@ class BlogLoginPage extends StatefulWidget {
 
 class _BlogLoginPageState extends State<BlogLoginPage> {
   @override
+  int type = 0; //0 登录 1 注册 2 找加密码
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(fit: StackFit.expand, alignment: Alignment.center, children: [
@@ -25,7 +26,7 @@ class _BlogLoginPageState extends State<BlogLoginPage> {
               filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
               child: Opacity(
                 opacity: 0.5,
-                child: Container(child: Text(" "), width: 360, height: 240),
+                child: Container(child: Text(" "), width: 360, height:  400),
               ),
             ),
           ),
@@ -41,70 +42,300 @@ class _BlogLoginPageState extends State<BlogLoginPage> {
                 ),
               ),
             )),
-        Center(
-            child: Container(
-          width: 360,
-          height: 200,
-          child: Column(
-            children: [
-              TextField(
-                  decoration: InputDecoration(
-                labelText: "用户名",
-                hintText: "用户名或者邮箱",
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-              )),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "密码",
-                  hintText: "你的登录密码",
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                ),
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  GestureDetector(
-                    child: Text("注册",
-                        style: TextStyle(
-                            color: Colors.white,
-                            decoration: TextDecoration.underline)),
-                  ),
-                  GestureDetector(
-                    child: Text("忘记密码",
-                        style: TextStyle(
-                            color: Colors.white,
-                            decoration: TextDecoration.underline)),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              RaisedButton(
-                onPressed: null,
-                child: Container(
-                  width: 240,
-                  child: Text(
-                    "登录",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 20,),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ))
+           getWidget(),
+
       ]),
     );
+  }
+
+  Widget getWidget() {
+    switch (type) {
+      case 1:
+        return buildRegisterView();
+      case 2:
+        return buildForgetPassword();
+      default:
+        return buildLoginView();
+    }
+  }
+
+  Widget buildLoginView() {
+    return Center(
+        child: Container(
+      width: 360,
+      height: 200,
+      child: Column(
+        children: [
+          TextField(
+              decoration: InputDecoration(
+            labelText: "用户名",
+            hintText: "用户名或者邮箱",
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+          )),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "你的登录密码",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    type = 1;
+                  });
+                },
+                child: Text("注册",
+                    style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline)),
+              ),
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    type = 2;
+                  });
+                },
+                child: Text("忘记密码",
+                    style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline)),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Container(
+              width: 240,
+              child: Text(
+                "登录",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget buildRegisterView() {
+    return Center(
+        child: Container(
+      width: 360,
+      height: 400,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Text("注册ZOE Blog账号",style: TextStyle(fontSize: 28,color: Colors.white),),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              decoration: InputDecoration(
+            labelText: "你的呢称",
+            hintText: "请输入呢称，注册后可随时修改",
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+          )),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "你的邮箱",
+              hintText: "你注册和找回密码使用的邮箱",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "你的登录密码",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            obscureText: true,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "密码确认",
+              hintText: "请再次输入一至密码",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Container(
+              width: 240,
+              child: Text(
+                "注册",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    type = 1;
+                  });
+                },
+                child: Text("返回登录",
+                    style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline)),
+              ),
+              GestureDetector(
+                child: Text("忘记密码",
+                    style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+
+  Widget buildForgetPassword() {
+    return Center(
+        child: Container(
+      width: 360,
+      height: 400,
+      child: Column(
+        children: [
+          Text("重设密码",style: TextStyle(fontSize: 28,color: Colors.white)),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              decoration: InputDecoration(
+            labelText: "你的邮箱",
+            hintText: "请输入你的邮箱用于获取重设码",
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+          )),
+          Row(
+            children: [
+              Text("输入你的注册邮箱后点击发送重设码,系统会发送一封邮件到你填写的邮箱里 ,你可以用邮件里的重设码来重设你的密码"),
+              RaisedButton(
+            onPressed: null,
+            child: Container(
+              width: 40,
+              child: Text(
+                "发送重设码",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          )
+            ],
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "你的邮箱",
+              hintText: "你注册和找回密码使用的邮箱",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "你的登录密码",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            obscureText: true,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "密码确认",
+              hintText: "请再次输入一至密码",
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Container(
+              width: 240,
+              child: Text(
+                "注册",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
